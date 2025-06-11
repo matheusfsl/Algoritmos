caa1ae5b220aa2b4c3c6e2ff7d5056047b9e5034
Matheus Fernandes <matiugaorion@hotmail.com>
2025-06-11 19:31:47 -0300

    melhorando o código

--- EstruturasLineares/src/main/java/Pilha/Pilha.java
+++ EstruturasLineares/src/main/java/Pilha/Pilha.java
@@ -2,46 +2,68 @@ package Pilha;
 
 
 public class Pilha {
-    private int [] elementos;
-    private  int topo;
+    private int[] elementos;
+    private int topo;
 
-    public Pilha(int capacidade){
+    public Pilha(int capacidade) {
         elementos = new int[capacidade];
         topo = -1;
     }
-    public void push (int valor){
-        if (topo == elementos.length-1){
+
+    public void push(int valor) {
+        if (isFull()) {
             throw new RuntimeException("A pilha se encontra cheia");
         }
-        elementos[++topo]=valor;
-
+        elementos[++topo] = valor;
     }
-    public int pop(){
-        if (isEmpty()){
+
+    public int pop() {
+        if (isEmpty()) {
             throw new RuntimeException("A pilha se encontra vazia");
         }
         return elementos[topo--];
     }
-    public int peek(){
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("A pilha se encontra vazia");
+        }
         return elementos[topo];
     }
-    public boolean isEmpty(){
-        if (topo== -1){
+
+    public boolean isFull() {
+        return topo == elementos.length - 1;
+    }
+
+    public boolean isEmpty() {
+        if (topo == -1) {
             return true;
         }
         return false;
     }
+    public void listarElementos() {
+        if (isEmpty()) {
+            System.out.println("A pilha está vazia.");
+            return;
+        }
+
+        System.out.println("Elementos da pilha (do topo para a base):");
+        for (int i = topo; i >= 0; i--) {
+            System.out.println(elementos[i]);
+        }
+    }
 
 
     public static void main(String[] args) {
-        Pilha pilha = new Pilha(3);
+        Pilha pilha = new Pilha(4);
         pilha.push(10);
         pilha.push(20);
         pilha.push(30);
+        pilha.push(40);
         pilha.pop();
-        System.out.println("Elemento do topo: "+ pilha.peek());
-
-
+        pilha.pop();
+        System.out.println("Elemento do topo: " + pilha.peek());
+        pilha.listarElementos();
     }
 }
 
